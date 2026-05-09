e6f7bc5c43b36fe72bc80f497a9897de4f10e0d3
Bilgesu Çakır <bilgesucakir@gmail.com>
2026-05-09 18:21:01 +0300

    Initial springboot configuration

---
+++ src/main/java/com/bilgesucakir/stitch_grapher/StitchGrapherApplication.java
@@ -0,0 +1,13 @@
+package com.bilgesucakir.stitch_grapher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StitchGrapherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StitchGrapherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bilgesucakir/stitch_grapher/StitchGrapherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilgesucakir.stitch_grapher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StitchGrapherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
